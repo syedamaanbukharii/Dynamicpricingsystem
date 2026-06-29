b34f0803d60791ffefd2bf806345f93e5c29d13d
Syed Amaan Bukhari <amaansyed285@gmail.com>
2026-06-30 00:00:57 +0530

    Fix formatting of docstring in __init__.py

--- app/pricing/__init__.py
+++ app/pricing/__init__.py
@@ -1,4 +1,4 @@
-"""Pricing engine and business-rules package."""
+"""Pricing engine and business-rules package.""
 
 from app.pricing.engine import PricingDecision, PricingEngine
 from app.pricing.rules import RuleResult, apply_business_rules, round_to_increment
